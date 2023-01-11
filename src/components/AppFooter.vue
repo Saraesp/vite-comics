3db437773f6ae1404517ba4fa1b84cdb77bd9585
Saraesp <saraesposito145@gmail.com>
2023-01-11 20:28:03 +0100

    impostazione struttura footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,68 @@
 <script>
 export default {
-        
+    data(){
+        return{
+            image: '/footer-bg.jpg'
+        }
+    },
+    methods: {
+        getImageUrl($img){
+            return new URL(`../assets/image/`)
+        }
+    }
+    
 }
 </script>
 
 <template lang="">
-<div>
+    <footer>
+        <div class="container">
+            <div class="background-prim">
+
+            </div>
+
+            <div class="background-image">
+                <h4></h4>
+                <nav>
+                    <ul>
+                        <li><a href=""></a></li>
+                    </ul>
+                </nav>
     
-</div>
+                <h4></h4>
+                <nav>
+                    <ul>
+                        <li><a href=""></a></li>
+                    </ul>
+                </nav>
+    
+                <h4></h4>
+                <nav>
+                    <ul>
+                        <li><a href=""></a></li>
+                    </ul>
+                </nav>
+    
+                <h4></h4>
+                <nav>
+                    <ul>
+                        <li><a href=""></a></li>
+                    </ul>
+                </nav>
+    
+            </div>
+    
+            <div class="background-second">
+    
+            </div>
+        </div>
+
+    </footer>
 </template>
 
-<style lang="">
+<style lang="scss">
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
 
 </style>
