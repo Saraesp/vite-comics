e912a1569fc47d866d7eeea0bb0ba807b5661dab
Saraesp <saraesposito145@gmail.com>
2023-01-12 18:25:13 +0100

    Immagine di bg e stile lista

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -41,44 +41,74 @@ export default {
 <template lang="">
     <footer>
             <div class="background-blue">
-                <div v-for="(item, index) in imagesBackgroundBlue" :key="index">
-                    <img :src="`/image/${item.image}.png`">
-                    <span>{{item.label}}</span>
+                <div class="container">
+                    <div class="cols">
+                        <div v-for="(item, index) in imagesBackgroundBlue" :key="index">
+                            <img :src="`/image/${item.image}.png`">
+                            <span>{{item.label}}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="background-image">
-                <div>
-                    <h4></h4>
-                    <nav>
-                        <ul>
-                            
-                        </ul>
-                    </nav>
-                </div>
+                <div class="container">
+                    <div class="footer-sx">
 
-                <div>
-                    <h4></h4>
-                    <nav>
-                        <ul>
-                            
-                        </ul>
-                    </nav>
-                </div>
+                    </div>
+                    <div class="col">
+                        <h4>DC COMICS</h4>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>Tv</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h4>SHOP</h4>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                    </div>
     
-                <div>
-                    <h4></h4>
-                    <nav>
-                        <ul>
-                            
-                        </ul>
-                    </nav>
+                    <div class="col">
+                        <h4>DC</h4>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy(New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                    </div>
+        
+                    <div class="col">
+                        <h4>SITES</h4>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>Dc Power Visa</li>
+                            </ul>
+                    </div>
+                     <div class="footer-dx">
+                        <img src="/image/dc-logo-bg.png" alt="">
+                    </div>
                 </div>
-    
             </div>
     
             <div class="background-black">
-    
+
             </div>
     </footer>
 </template>
@@ -88,17 +118,60 @@ export default {
     @use '../styles/partials/mixins' as *;
 
     .background-blue{
-        height: 100px;
-        display: flex;
-        justify-content: space-between;
+        height: 150px;
         background-color: $color-blue;
+        
+        .cols{
+            @include center;
+            justify-content: space-between;
 
-        span{
-            text-transform: uppercase;
-            color: white;
-            vertical-align: middle;
+            span{
+                text-transform: uppercase;
+                color: white;
+                vertical-align: middle;
+            }
+        }
+
+    }
+
+    .background-image{
+        background-image: url('/image/footer-bg.jpg');
+        background-size: cover;
+
+        .container{
+            background-image: url('/img/dc-logo-bg.png');
+            background-position: right center;
+            background-repeat: no-repeat;
+            display: flex;
+            .footer-sx{
+                display: flex;
+                margin: 20px;
+            }
+
+            .col{
+                margin: 60px 5px;
+                line-height: 2;
+
+                li{
+                    color: rgb(167, 163, 163);
+                    font-size: 12px;
+                }
+
+                h4{
+                    color: white;
+                    text-transform: uppercase;
+                    font-weight: 800;
+                    font-family: 'Roboto', sans-serif;
+                }
+
+                ul{
+                    list-style: none;
+                }
+            }
         }
     }
+
+
 
 
 
